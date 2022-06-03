a51dff87fc4c5b82cbcd36fdaa9346a065aafd89
Chris Petrone <ctpetrone@gmail.com>
2022-06-03 11:26:20 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,33 @@ locals {
   domain = "react-aws-terraform-github-actions.chrispetrone.com"
   s3_origin_id = "s3-react-aws-terraform-github-actions"
 }
+
+data "aws_iam_policy_document" "s3-website-policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket" "react-aws-terraform-github-actions-s3-bucket" {
+  bucket = var.bucket_name
+  acl = "public-read"
+  policy = data.aws_iam_policy_document.s3-website-policy.json
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "react-aws-terraform-github-actions-s3-access-control" {
+  bucket = aws_s3_bucket.react-aws-terraform-github-actions-s3-bucket.id
+  block_public_acls   = true
+  ignore_public_acls = true
+}
