a557863752a9a74f77d52e66f6bf24a21a7c0845
Chris Petrone <ctpetrone@gmail.com>
2023-03-02 16:17:23 -0800

    added cv

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,10 @@
 import React from 'react';
 const Resume = () => {
     return (
-        <div>
-            <h3>Dogs</h3>
-            <div>
-                RESUMEEE
-            </div>
+        <div height="100%">
+            <object data="content/Christopher_Petrone_Resume.pdf" type="application/pdf" width="100%" height="800px">
+      <p>Alternative text - include a link <a href="content/Christopher_Petrone_Resume.pdf">to the PDF!</a></p>
+  </object> 
         </div>
     );
 }
