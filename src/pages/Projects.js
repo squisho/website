1f768c8f016d9eb3355c979d84765d843b51024d
Chris Petrone <ctpetrone@gmail.com>
2024-01-31 15:28:38 -0800

    Added TreeMap to my portfolio.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,9 +8,23 @@ const textStyle = {
 const Projects = () => {
     return (
         <Grid className="portfolio" name="portfolio" container direction="column" padding={3} style={{"marginTop":"-1px"}}> 
-                <Grid container item direction="row" justifyContent="center" alignItems='center' name="DevXP">
-                    <Grid item style={{display:'flex', justifyContent:'center'}} xs={3}> 
-                        <a href="https://devxp.ca/">
+
+            <Grid item container direction="row" name="treemap" justifyContent="center" alignItems="center">
+                <Grid item xs={8}>
+                        <h3>TreeMap</h3>
+                        <p style={{fontSize:16}}>And interactive map combining BC Data Catalogue forestry & park GIS databases with logging tenures, to help make the data more publically available.<br/><br/>Data was selected from the past 6 years, cleaned and processed using mapshaper.js and layered over MapBox-GL base maps.</p>
+                </Grid>
+                <Grid item style={{display:'flex', justifyContent:'center'}} xs={3}>
+                        <a href="https://squisho.github.io/treemap/index.html" target="_blank">
+                        <img width="75%" src="content/treemap.png" alt="InfoVid Webapp"></img>
+                        </a>
+                </Grid>
+
+            </Grid>
+
+            <Grid container item direction="row" justifyContent="center" alignItems='center' name="DevXP">
+                <Grid item style={{display:'flex', justifyContent:'center'}} xs={3}> 
+                        <a href="https://devxp.ca/" target="_blank">
                         <img width="75%" src="content/devxp.png" alt="DevXP Logo"></img>
                         </a>
                     </Grid>
@@ -27,7 +41,7 @@ const Projects = () => {
                         <p style={{fontSize:16}}>Designed and built a data visualization to explore real estate demographics and projections within communities in the CRD.  Data was pulled from the CMHC, provincial and municipal government databases, and local real estate organizations.  Data was analyzed and engineered in Python, and exported for use in a custom, interactive D3.js visualization.</p>
                     </Grid>
                     <Grid item style={{display:'flex', justifyContent:'center'}} xs={3}>
-                        <a href="https://github.com/squisho/infovis">
+                        <a href="https://github.com/squisho/infovis" target="_blank">
                         <img width="75%" src="content/infovis.png" alt="InfoVid Webapp"></img>
                         </a>
                     </Grid>
@@ -50,7 +64,7 @@ const Projects = () => {
                         <p style={{fontSize:16}}> SoundBoy is a tool to help music producers automatically manage and organize their sample libraries.  Trained on a custom set of nearly 100,000 samples, it achieved an 87% success rate when tested on independent sample sets.  I was responsible for acquiring, cleaning and categorizing the datasets.  I also designed, built, and tested several models.  This classifier was embeded within an Electrum application and was successful in classifying large sets of user samples.</p>
                     </Grid>
                     <Grid item style={{display:'flex', justifyContent:'center'}} xs={3}>
-                        <a href="https://github.com/julesyoungberg/soundboy">
+                        <a href="https://github.com/julesyoungberg/soundboy" target="_blank">
                         <img width="75%" src="content/soundboy.png" alt="DevXP Logo"></img>
                         </a>
                     </Grid>
